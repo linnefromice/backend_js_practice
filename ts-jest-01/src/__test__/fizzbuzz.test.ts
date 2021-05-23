7dc90d732a7fcef08908f461a4856c4ac9e9fa92
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-05-23 13:23:00 +0900

    update fizzbuzz tests

--- ts-jest-01/src/__test__/fizzbuzz.test.ts
+++ ts-jest-01/src/__test__/fizzbuzz.test.ts
@@ -4,4 +4,7 @@ describe("FizzBuzzは", () => {
   test('3の倍数を受け取るとFizzを返す', () => {
     expect(new FizzBuzz().execute(3)).toEqual("Fizz");
   });
+  test('5の倍数を受け取るとBuzzを返す', () => {
+    expect(new FizzBuzz().execute(5)).toEqual("Buzz");
+  });
 });
