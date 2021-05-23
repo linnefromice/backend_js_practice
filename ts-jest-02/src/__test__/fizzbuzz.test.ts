686d8c49028b4751ba134658a2cb1efbd6048dea
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-05-23 17:23:46 +0900

    case 3->Fizz - temp green & refactoring for test codes

--- ts-jest-02/src/__test__/fizzbuzz.test.ts
+++ ts-jest-02/src/__test__/fizzbuzz.test.ts
@@ -1,7 +1,19 @@
 import { FizzBuzz } from "../fizzbuzz";
 
-test('1を渡すと文字列1を返す', () => {
-  const fizzbuzz = new FizzBuzz();
-  expect("1").toEqual(fizzbuzz.convert(1));
+let fizzbuzz: FizzBuzz;
+describe('FizzBuzz', () => {
+  beforeEach(() => {
+    fizzbuzz = new FizzBuzz();
+  });
+  test('1を渡すと文字列1を返す', () => {
+    expect("1").toEqual(fizzbuzz.convert(1));
+  });
+  test('2を渡すと文字列2を返す', () => {
+    expect("2").toEqual(fizzbuzz.convert(2));
+  });
+  test('3を渡すと文字列Fizzを返す', () => {
+    expect("Fizz").toEqual(fizzbuzz.convert(3));
+  });  
 });
+
 
--- ts-jest-02/src/fizzbuzz.ts
+++ ts-jest-02/src/fizzbuzz.ts
@@ -1,5 +1,6 @@
 export class FizzBuzz {
-  public convert = (arg: number): string => {
-    return "1"
+  public convert = (num: number): string => {
+    if (num === 3) return "Fizz";
+    return String(num);
   }
 }
