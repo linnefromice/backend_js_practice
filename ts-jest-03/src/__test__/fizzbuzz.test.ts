6f6d1166cd28ed00be95f903ee1e5361baa0ecf3
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-05-24 11:48:10 +0900

    case fizzbuzz

--- ts-jest-03/src/__test__/fizzbuzz.test.ts
+++ ts-jest-03/src/__test__/fizzbuzz.test.ts
@@ -10,11 +10,22 @@ describe('FizzBuzz', () => {
       test('3を渡すと文字列Fizzを返す', () => {
         expect("Fizz").toEqual(fizzbuzz.convert(3));
       });
+      test('6を渡すと文字列Fizzを返す', () => {
+        expect("Fizz").toEqual(fizzbuzz.convert(6));
+      });
     });
-    describe('5の倍数の時は数の代わりに"Buzz"に変換する', () => {
+    describe('同値類の中の最小の5の倍数5の時は数の代わりに"Buzz"に変換する', () => {
       test('5を渡すと文字列Buzzを返す', () => {
         expect("Buzz").toEqual(fizzbuzz.convert(5));
-      });  
+      });
+      test('上限の境界値100を渡すと文字列Buzzを返す', () => {
+        expect("Buzz").toEqual(fizzbuzz.convert(10));
+      });
+    });
+    describe('3と5両方の倍数の時はFizzBuzzに変換する', () => {
+      test('同値類の中の最小の3と5の公倍数15を渡すと文字列"FizzBuzz"を返す', () => {
+        expect("FizzBuzz").toEqual(fizzbuzz.convert(15));
+      });
     });
     describe('その他の数はそのまま文字列に変換する', () => {
       test('1を渡すと文字列1を返す', () => {
--- ts-jest-03/src/fizzbuzz.ts
+++ ts-jest-03/src/fizzbuzz.ts
@@ -1,5 +1,6 @@
 export class FizzBuzz {
   public convert = (num: number): string => {
+    if (num % 3 === 0 && num % 5 === 0) return "FizzBuzz";
     if (num % 3 === 0) return "Fizz";
     if (num % 5 === 0) return "Buzz";
     return String(num);
