736bb4f6444825594399129415c94f7ae281c43a
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-05-24 13:07:28 +0900

    use parameterized test

--- ts-jest-03/src/__test__/fizzbuzz.test.ts
+++ ts-jest-03/src/__test__/fizzbuzz.test.ts
@@ -7,19 +7,13 @@ describe('FizzBuzz', () => {
   });
   describe("convertメソッドは数を文字列に変換する", () => {
     describe('3の倍数の時は数の代わりに"Fizz"に変換する', () => {
-      test('3を渡すと文字列Fizzを返す', () => {
-        expect("Fizz").toEqual(fizzbuzz.convert(3));
-      });
-      test('6を渡すと文字列Fizzを返す', () => {
-        expect("Fizz").toEqual(fizzbuzz.convert(6));
+      test.each([3, 6, 99])("%iを渡すと文字列Fizzを返す", (input) => {
+        expect("Fizz").toEqual(fizzbuzz.convert(input));
       });
     });
     describe('同値類の中の最小の5の倍数5の時は数の代わりに"Buzz"に変換する', () => {
-      test('5を渡すと文字列Buzzを返す', () => {
-        expect("Buzz").toEqual(fizzbuzz.convert(5));
-      });
-      test('上限の境界値100を渡すと文字列Buzzを返す', () => {
-        expect("Buzz").toEqual(fizzbuzz.convert(10));
+      test.each([5, 10, 100])("%iを渡すと文字列Buzzを返す", (input) => {
+        expect("Buzz").toEqual(fizzbuzz.convert(input));
       });
     });
     describe('3と5両方の倍数の時はFizzBuzzに変換する', () => {
@@ -31,11 +25,8 @@ describe('FizzBuzz', () => {
       });
     });
     describe('その他の数はそのまま文字列に変換する', () => {
-      test('1を渡すと文字列1を返す', () => {
-        expect("1").toEqual(fizzbuzz.convert(1));
-      });
-      test('2を渡すと文字列2を返す', () => {
-        expect("2").toEqual(fizzbuzz.convert(2));
+      test.each([1, 2, 98])("%iを渡すと文字列Fizzを返す", (input) => {
+        expect(String(input)).toEqual(fizzbuzz.convert(input));
       });
       test('上限の境界値の1つ外側の101を渡すと文字列101を返す', () => {
         expect("101").toEqual(fizzbuzz.convert(101));
